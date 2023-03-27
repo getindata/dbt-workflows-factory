a6dd6e48b8270acfb35d798fb471e5de7c8cb0ce
Anita Śledź <anita.sledz@getindata.com>
2023-03-27 07:59:44 +0200

    Update src/dbt_workflows_converter/compile.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/dbt_workflows_converter/compile.py
+++ src/dbt_workflows_converter/compile.py
@@ -4,17 +4,55 @@ from params import Params
 
 
 @click.command()
-@click.argument('manifest_file', type=click.Path(exists=True))
-@click.option('--image-uri', type=str, help='Docker image URI', required=True)
-@click.option('--region', type=str, help='GCP region', required=True)
-@click.option('--full-command', type=str, help='Full command to run in container', required=True)
-@click.option('--remote-path', type=str, help='Path to remote file', required=True)
-@click.option('--key-volume-mount-path', type=str, help='Volume mount path for private key', required=True)
-@click.option('--key-volume-path', type=str, help='Path for the private key file on the host', required=True)
-@click.option('--key-path', type=str, help='Path for the private key file in the container', required=True)
-def convert(manifest_file, image_uri, region, full_command, remote_path, key_volume_mount_path, key_volume_path,
-            key_path):
-    params = Params(image_uri=image_uri, region=region, full_command=full_command, remote_path=remote_path, key_volume_mount_path=key_volume_mount_path, key_volume_path=key_volume_path, key_path=key_path)
+@click.argument("manifest_file", type=click.Path(exists=True))
+@click.option("--image-uri", type=str, help="Docker image URI", required=True)
+@click.option("--region", type=str, help="GCP region", required=True)
+@click.option(
+    "--full-command",
+    type=str,
+    help="Full command to run in container",
+    required=True,
+)
+@click.option(
+    "--remote-path", type=str, help="Path to remote file", required=True
+)
+@click.option(
+    "--key-volume-mount-path",
+    type=str,
+    help="Volume mount path for private key",
+    required=True,
+)
+@click.option(
+    "--key-volume-path",
+    type=str,
+    help="Path for the private key file on the host",
+    required=True,
+)
+@click.option(
+    "--key-path",
+    type=str,
+    help="Path for the private key file in the container",
+    required=True,
+)
+def convert(
+    manifest_file,
+    image_uri,
+    region,
+    full_command,
+    remote_path,
+    key_volume_mount_path,
+    key_volume_path,
+    key_path,
+):
+    params = Params(
+        image_uri=image_uri,
+        region=region,
+        full_command=full_command,
+        remote_path=remote_path,
+        key_volume_mount_path=key_volume_mount_path,
+        key_volume_path=key_volume_path,
+        key_path=key_path,
+    )
     # params = {
     #     'image_uri': image_uri,
     #     'region': region,
