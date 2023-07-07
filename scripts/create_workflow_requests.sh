fb5b4841de7729cf44c621df29adb1575a296b4c
Piotr Tutak <piotr.tutak@getindata.com>
2023-07-07 15:33:02 +0200

    script example

--- scripts/create_workflow_requests.sh
+++ scripts/create_workflow_requests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 python -m dbt_workflows_factory.cli convert \
-    --image-uri europe-central2-docker.pkg.dev/gid-dataops-labs/composer-dags/dbt-tpch@sha256:c3006e2723faaaaec06dbad556c776e8a0b3d9e71fd0159cebf865fb7ce14540 \
+    --image-uri my-image-uri \
     --region europe-west6 \
-    --gcs-key-volume-remote-path gs://dataops-dev-state/bq-dataops-dev-342817.json \
+    --gcs-key-volume-remote-path gs/key/path.json \
     --gcs-key-volume-mount-path /etc/gcs-key/ \
     --gcs-key-volume-container-mount-path /etc/gcs-key/:/etc/gcs-key/:ro \
-    --container-gcp-key-path /etc/gcs-key/bq-dataops-dev-342817.json \
+    --container-gcp-key-path /etc/gcs-key/path.json \
     --container-gcp-project-id gid-dataops-labs \
     --pretty \
     tests/unit/dbt_workflows_factory/test_data/manifest.json > workflow_source.json
@@ -15,7 +15,6 @@ python -m dbt_workflows_factory.cli create-request \
     --name test-workflow-factory-1 \
     --service-account my-service-account \
     --source-path workflow_source.json > workflow_request.json
-
 
 # curl --request POST \
 #   'https://workflows.googleapis.com/v1/projects/project_id/locations/location_id/workflows?workflowId=my-workflow&key=[YOUR_API_KEY]' \
